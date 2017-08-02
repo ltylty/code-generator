e8e9bc7a4815f653cecc94f216c18abe12f8d625
Liu Tian Yuan <toy_liu@enable-ets.com>
2017-08-02 14:11:26 +0800

    autoCommit

--- src/main/resources/velocity/${className}Service.java
+++ src/main/resources/velocity/${className}Service.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.enableets.edu.framework.core.service.ServiceAdapter;
-import com.wistron.swpc.framework.util.BeanUtils;
+import org.springframework.beans.BeanUtils;
 import com.enableets.edu.${moudleName}.framework.po.${className}PO;
 import com.enableets.edu.${moudleName}.microservice.bo.${className}BO;
 
